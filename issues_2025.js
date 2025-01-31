0097fc57c1168e7e97cd6a6cd22e71058823d5dc
Billie Zhang <billiecoder@gmail.com>
2025-01-31 21:03:46 +0800

    i

--- issues_2025.js
+++ issues_2025.js
@@ -1,13 +1,23 @@
 export const issues = {
-  Jan29: {
+  Jan31: {
     bird_box: {
-      desc: "design in sketchup",
-      time: 8
+      desc: "assemble the box and play with sketchup",
+      time: 6
+    },
+    housework: {
+      desc: "cook, do the laundry, wash dishes and mop the kitchen floor",
+      time: 2
     }
   },
   Jan30: {
     bird_box: {
       desc: "cut wood boards and play with sketchup",
+      time: 8
+    }
+  },
+  Jan29: {
+    bird_box: {
+      desc: "design in sketchup",
       time: 8
     }
   }
